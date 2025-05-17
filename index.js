f86a232b02b5fb266dbd6d655579a75b3821a743
Shivam Kumar <kumarshivam16062004@gmail.com>
2025-05-17 23:10:54 +0530

    chapter 2

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-// import {add,diff} from './lib.js';
-// console.log(add(1,2),diff(2,1));
+const http = require('http')
+const fs = require('fs');
+const indexpage = fs.readFileSync('./index.html','utf-8');
+const page404 = fs.readFileSync('./page404.html','utf-8');
+const datas = JSON.parse(fs.readFileSync('./data.json','utf-8'));
+const products = datas.products;
+console.log(products);
+const server = http.createServer((req,res)=>{
+    console.log(req.url);
+    console.log('server started');
 
-// const lib = require('./lib');
-// const fs = require('fs');
+    if(req.url.startsWith('/product')){
+        const id = (req.url.split('/'))[2];
+        console.log(id);
+        let product = products.find(p=>p.id===(+id));
 
+        console.log(product);
+        res.setHeader('content-type','text/html');
+            let updatedpage = indexpage;
+            updatedpage=updatedpage.replace('**title**',product.title);
+            updatedpage=updatedpage.replace('**description**',product.description);
+            updatedpage=updatedpage.replace('**availabilityStatus**',product.availabilityStatus);
+            updatedpage=updatedpage.replace('**price**',product.price);
+            updatedpage=updatedpage.replace('**rating**',product.reviews[0].rating.toFixed(1));
+            updatedpage=updatedpage.replace('**thumbnail**',product.thumbnail);
+            res.end(updatedpage);
+            return;
+    }
+    switch(req.url){
+        case '/':
+            res.setHeader('content-type','text/html');
+            res.end(indexpage);
+            break;
+        case '/api':
+            res.setHeader('content-type','application/json');
+            res.end(JSON.stringify(datas));
+            break;
 
-
-
-// const p1 = performance.now();
-// fs.readFile('demo.txt','utf-8',(err,data)=>{
-//     console.log(data);
-// })
-
-// const p2 = performance.now();
-// console.log(lib.add(59,10),lib.diff(70,1));
-// console.log(p2-p1);
-
-
-const express = require('express');
-
-
-const server = express();
-console.log('server is running on port ');
-
-server.listen(9000);
-
+        default:
+            res.statusCode = 404;
+            res.setHeader('content-type','text/html');
+            res.end(page404);
+    }
+})
+server.listen(8080);
